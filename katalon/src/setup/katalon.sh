55f59bb3a394b880180e879f4282e2dcddf31649
Ayush <ayush15may@gmail.com>
2019-09-20 20:33:51 +0530

    katalon-node image

--- katalon/src/setup/katalon.sh
+++ katalon/src/setup/katalon.sh
@@ -4,14 +4,22 @@ set -xe
 
 echo "Install Katalon"
 
-version=$KATALON_STUDIO_VERSION
+version=6.3.3
 directory=$version
 package=Katalon_Studio_Linux_64-$version.tar.gz
 unzipped_directory=Katalon_Studio_Linux_64-$version
 
-wget -O $package https://github.com/katalon-studio/katalon-studio/releases/download/v$version/Katalon_Studio_Linux_64-$version.tar.gz
+# wget -O $package https://github.com/katalon-studio/katalon-studio/releases/download/v$version/Katalon_Studio_Linux_64-$version.tar.gz
+wget -O $package https://download.katalon.com/6.3.3/Katalon_Studio_Linux_64-6.3.3.tar.gz
+
 ls
 tar -xvzf $package -C $KATALON_KATALON_INSTALL_DIR_PARENT
+# mkdir $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory
+# mkdir $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory/katalon
+# mkdir $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory/configuration
+# mkdir $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory/configuration/resources
+# mkdir $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory/configuration/resources/drivers
+
 ls
 rm $package
 
